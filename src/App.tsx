57b22950e6576f9288a11c91128edcedc9641ec7
sabi <glowswin@naver.com>
2022-02-22 21:43:36 +0900

    styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
+const Father = styled.div`
+  font-size: 15px;
+`;
+const BoxOne = styled.div`
+  background-color: teal;
+  color: white;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled(BoxOne)`
+  background-color: tomato;
+`;
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Father>
+      <BoxOne />
+      <BoxTwo />
+    </Father>
+  );
 }
 
 export default App;
