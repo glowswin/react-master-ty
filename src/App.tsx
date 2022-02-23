a168cd3791719436835ade357cb47db895299e20
sabi <glowswin@naver.com>
2022-02-23 20:42:10 +0900

    attr

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Circle from "./Circle";
 
 const Father = styled.div`
@@ -14,11 +14,45 @@ const BoxOne = styled.div`
 const BoxTwo = styled(BoxOne)`
   background-color: tomato;
 `;
+const anim = keyframes`
+  0%{
+    color:red;
+  }
+  60%{
+    color:black;
+  }
+  100%{
+    color:yellow;
+  }
+`;
+const SpanText = styled.span`
+  animation: ${anim} 5s infinite;
+`;
+const InputBox = styled.input.attrs({ require: true, maxLength: 20 })`
+  background-color: #c9c9c9;
+`;
+const Btn = styled.button`
+  padding: 20px;
+  background-color: red;
+  color: white;
+  ${SpanText} {
+    &:hover {
+      font-size: 100px;
+    }
+  }
+`;
 function App() {
   return (
     <Father>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Circle bgColor="teal" borderColor="yellow" />
+      <Circle bgColor="tomato" text="new Text" />
+      <InputBox />
+      <Btn>
+        <SpanText>유후후후</SpanText>
+      </Btn>
+      <Btn as="a">
+        <SpanText>dkjfkdjkf</SpanText>
+      </Btn>
     </Father>
   );
 }
