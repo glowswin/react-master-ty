24a9c2db049aff9c6cc806f94896ed3f2e64ae78
sabi <glowswin@naver.com>
2022-02-23 22:17:56 +0900

    coin info

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,79 @@
-import React from "react";
-import styled, { keyframes } from "styled-components";
-import Circle from "./Circle";
+import { createGlobalStyle } from "styled-components";
+import Router from "./Router";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-const Father = styled.div`
-  font-size: 15px;
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
+}
+body {
+  line-height: 1;
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+* {
+  box-sizing: border-box;
+}
+body {
+  font-weight: 300;
+  font-family: 'Source Sans Pro', sans-serif;
+  background-color:${(props) => props.theme.bgColor};
+  color:${(props) => props.theme.textColor};
+  line-height: 1.2;
+}
+a {
+  text-decoration:none;
+  color:inherit;
+}
 `;
-const BoxOne = styled.div`
-  background-color: teal;
-  color: white;
-  width: 100px;
-  height: 100px;
-`;
-const BoxTwo = styled(BoxOne)`
-  background-color: tomato;
-`;
-const anim = keyframes`
-  0%{
-    color:red;
-  }
-  60%{
-    color:black;
-  }
-  100%{
-    color:yellow;
-  }
-`;
-const SpanText = styled.span`
-  animation: ${anim} 5s infinite;
-`;
-const InputBox = styled.input.attrs({ require: true, maxLength: 20 })`
-  background-color: #c9c9c9;
-`;
-const Btn = styled.button`
-  padding: 20px;
-  background-color: red;
-  color: white;
-  ${SpanText} {
-    &:hover {
-      font-size: 100px;
-    }
-  }
-`;
+
 function App() {
   return (
-    <Father>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" text="new Text" />
-      <InputBox />
-      <Btn>
-        <SpanText>유후후후</SpanText>
-      </Btn>
-      <Btn as="a">
-        <SpanText>dkjfkdjkf</SpanText>
-      </Btn>
-    </Father>
+    <>
+      <GlobalStyle />
+      <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </>
   );
 }
 
